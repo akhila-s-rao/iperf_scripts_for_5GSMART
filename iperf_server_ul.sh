88b65635794469e77ce9149419e98e068409775c
Akhila Rao <akhila.rao@sics.se>
2021-02-18 18:41:14 +0100

    Added server side scripts

---
+++ iperf_server_ul.sh
@@ -0,0 +1,12 @@
+num_steps=3
+start_port_num=11000
+report_interval=0.01   # seconds
+
+for i in {0..2} # CHANGE THIS TO num_steps-1
+do
+	echo $i
+	port_num=$(($start_port_num+$i))
+	echo 'Starting server at port_num ' $port_num
+	iperf -s -u -i $report_interval -p $port_num -e -y C > iperf_server_ul_stream$i.log &
+	sleep 0.1
+done
